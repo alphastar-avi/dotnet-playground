95e9636b220fe58aa1884ac88a2c9a54ec1a0f34
Avinash <avinash592004@gmail.com>
2026-01-19 22:44:34 +0530

    cliSum

---
+++ cliSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        Console.WriteLine("Enter 5 numbers:");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Number {i}: ");
+            int num = int.Parse(Console.ReadLine());
+            sum += num;
+        }
+
+        Console.WriteLine($"Sum = {sum}");
+    }
+}
